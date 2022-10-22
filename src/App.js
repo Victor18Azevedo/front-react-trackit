f4be306caa98de697fad889c874b9d4e7be512ff
Victor Azevedo <josevcazevedo@gmail.com>
2022-10-22 11:18:33 -0300

    Implemented HistoryPage with Calendar

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ function App() {
     }
   }, []);
 
+  console.log("App");
+
   return (
     <BrowserRouter>
       <GlobalStyle />
--- src/pages/HistoryPage/HistoryPage.js
+++ src/pages/HistoryPage/HistoryPage.js
@@ -2,8 +2,75 @@ import styled from "styled-components";
 import Header from "../../components/Header";
 import Menu from "../../components/Menu";
 import { baseColor } from "../../constants/colors";
+import React, { useContext, useEffect, useRef, useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import dayjs from "dayjs";
+import UserContext from "../../contexts/UserContext";
+import { BASE_URL } from "../../constants/urls";
+import axios from "axios";
 
 export default function HistoryPage() {
+  const { localUser } = useContext(UserContext);
+  const [value, setValue] = useState(new Date());
+  const [history, setHistory] = useState([]);
+  const [progressPerDay, setProgressPerDay] = useState([]);
+  const habitsDays = useRef([]);
+
+  const calcProgress = function (history) {
+    return history.map((d) => {
+      const habitsNumber = d.habits.length;
+      const habitsDoneNumber = d.habits.filter((h) => h.done).length;
+      const dayProgress = parseInt((habitsDoneNumber / habitsNumber) * 100);
+      return {
+        day: d.day,
+        progress: dayProgress,
+      };
+    });
+  };
+
+  useEffect(() => {
+    if (history.length > 0) {
+      const newProgressPerDay = calcProgress(history);
+      setProgressPerDay([...newProgressPerDay]);
+      habitsDays.current = newProgressPerDay.map((d) => d.day);
+    }
+  }, [history]);
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${localUser.token}`,
+      },
+    };
+    axios
+      .get(`${BASE_URL}/habits/history/daily`, config)
+      .then((res) => {
+        setHistory([...res.data]);
+      })
+      .catch((err) => {
+        alert(err.response.data);
+        console.log(err.response);
+      });
+  }, []);
+
+  function onChange(nextValue) {
+    setValue(nextValue);
+  }
+
+  function tileClassName({ date, view }) {
+    const calendarDate = dayjs(date).format("DD/MM/YYYY");
+    if (view === "month") {
+      if (habitsDays.current?.includes(calendarDate)) {
+        const matchDay = progressPerDay.filter(
+          (d) => d.day === calendarDate
+        )[0];
+        return matchDay.progress === 100 ? "complete" : "incomplete";
+      }
+    }
+    return "";
+  }
+
   return (
     <ContainerHistoryPage>
       <Header />
@@ -11,7 +78,17 @@ export default function HistoryPage() {
         <div className="box-title">
           <h2>Histórico</h2>
         </div>
-        <p>Em breve você poderá ver o histórico dos seus hábitos aqui!</p>
+        <div>
+          <Calendar
+            className="calendar"
+            onChange={onChange}
+            value={value}
+            minDate={new Date(2020, 0)}
+            maxDate={new Date(2029, 0)}
+            minDetail={"year"}
+            tileClassName={tileClassName}
+          />
+        </div>
       </MainHistory>
       <Menu />
     </ContainerHistoryPage>
@@ -30,5 +107,23 @@ const MainHistory = styled.main`
   background-color: ${baseColor};
   .box-title {
     margin: 22px 0 20px;
+  }
+  .calendar {
+    margin: 0 auto;
+    border: none;
+    border-radius: 10px;
+    box-shadow: 0 5px 10px #333;
+  }
+
+  .complete {
+    background-color: #8cc654;
+    color: #111;
+    border-radius: 50%;
+  }
+
+  .incomplete {
+    background-color: #ea5766;
+    color: #111;
+    border-radius: 50%;
   }
 `;
